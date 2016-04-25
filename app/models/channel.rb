d31d88be69295550e1444d79718a43e66818af6f
Joe Sak <joe@joesak.com>
2016-04-25 18:50:02 -0400

    Fallback to all on grouped_by_created_year

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,7 +1,7 @@
 class Channel < ActiveRecord::Base
   scope :recently_updated_on_top, -> { order('updated_at DESC') }
 
-  def self.grouped_by_created_year(channels)
+  def self.grouped_by_created_year(channels = all)
     channels.group_by { |channel| channel.created_at.year }
   end
 
