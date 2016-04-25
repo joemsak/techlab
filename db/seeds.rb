6cbb8cfaef7016460e9da3656f624e41aa831acd
Joe Sak <joe@joesak.com>
2016-04-25 13:39:17 -0400

    Sync the channels in the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+$stdout.write("Syncing channels from AudioAddict...\n")
+
+sync = SynchronizeChannels.new
+
+sync.call do |channel|
+  $stdout.write("Created #{channel.name}\n")
+end
