780d6d1a756c3118ad7318976354e7fe3c106efe
Joe Sak <joe@joesak.com>
2016-04-25 18:46:53 -0400

    Replace a faux scope with a class method

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,11 +1,9 @@
 class Channel < ActiveRecord::Base
   scope :recently_updated_on_top, -> { order('updated_at DESC') }
 
-  scope :grouped_by_created_year, ->(collection) {
-    collection.group_by do |channel|
-      channel.created_at.year
-    end
-  }
+  def self.grouped_by_created_year(channels)
+    channels.group_by { |channel| channel.created_at.year }
+  end
 
   def self.storage_attrs
     %i{api_id
